245bce8cd7e3af52d19b267513510a2fe8b6ed26
Екатерина Широбокова <e.shirobokova@ddxfitness.local>
2023-06-06 01:11:34 +0300

    add clubs and payment plans tests

---
+++ tests/paymentPlans.test.ts
@@ -0,0 +1,22 @@
+import { expect, request, test } from "@playwright/test";
+import api from './api.json';
+
+test.describe("API-тесты на получение списка тарифов", async () => {
+    test("[positive] получить список тарифов", async ({ request }) => {
+       const response = await request.get(
+        `${api.urls.base_url_api}${api.paths.paymentPlans}`,
+        {
+            headers: {
+                'Authorization': `${api.tokens.test}`
+            },
+            params: {
+                session_id:"1",
+                request_id:"2",
+                "request_source": "web app",
+                is_active: true
+            }
+        }
+       );
+       expect(response.status()).toEqual(200);
+    });
+});
